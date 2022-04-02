1a39f54d9f595449060406926cdd328f0407feff
durianpudding <durianpuddingupup@gmail.com>
2022-04-03 01:00:40 +0200

    quoted url

--- scripts/set_webhook.sh
+++ scripts/set_webhook.sh
@@ -1,1 +1,1 @@
-curl "https://api.telegram.org/bot$BOT_TOKEN/setWebhook?url=https://$HEROKU_APP_NAME.herokuapp.com/updateHook/$WEBHOOK_TOKEN&allowed_updates=[\"my_chat_member\"]"
+curl "https://api.telegram.org/bot$BOT_TOKEN/setWebhook?url=https://$HEROKU_APP_NAME.herokuapp.com/updateHook/$WEBHOOK_TOKEN&allowed_updates=%5B%22my_chat_member%22%5D"
